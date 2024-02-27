bfe9a4818717ac29914d6ae04323e9d9db6e24de
Giuseppe Valeriani <giuggey85@gmail.com>
2024-02-27 17:03:24 +0000

    add routing

---
+++ app/pupils/[id]/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const page = ({ params }) => {
+  console.log(params);
+  return <div>nice</div>;
+};
+
+export default page;
--- components/PupilsList/PupilsList.jsx
+++ components/PupilsList/PupilsList.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
 import "./PupilsList.scss";
 
 const PupilsList = () => {
-  return <section>ciao</section>;
+  const [fetchedList, setFetchedList] = useState(null);
+
+  useEffect(() => {
+    const getList = async () => {
+      const response = await axios.get("http://localhost:7777/");
+      setFetchedList(response.data);
+    };
+    getList();
+  }, []);
+
+  if (!fetchedList) {
+    return <section className="loading">Loading...</section>;
+  }
+
+  return (
+    <section className="pupils-list">
+      <h2 className="pupils-list__title">Pupils List</h2>
+      <ul className="pupils-list__list">
+        {fetchedList.map((pupil) => (
+          <li className="pupils-list__element" key={pupil.id}>
+            <Link className="pupils-list__pupil" href={`/pupils/${pupil.id}`}>
+              {pupil.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default PupilsList;
